9b3c16c16ba7899553740544f380f826e16c683c
mahf708 <naser.mahfouz@pnnl.gov>
2024-06-02 16:58:33 -0400

    updates recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,23 @@
 #!/bin/env bash
 
+set -exuo pipefail
+
+export TGT_MAC=${MY_MACHINE:-else}
+
 cd scream/components/eamxx/src/python
-# download this file because otherwise the build will fail...
-wget https://web.lcrc.anl.gov/public/e3sm/inputdata/atm/scream/tables/p3_lookup_table_2.dat-v4.1.1
-wget https://web.lcrc.anl.gov/public/e3sm/inputdata/atm/scream/tables/p3_lookup_table_1.dat-v4.1.1
-mkdir -p build_src/atm/scream/tables
-mv p3_lookup_table_1.dat-v4.1.1 build_src/atm/scream/tables/
-mv p3_lookup_table_2.dat-v4.1.1 build_src/atm/scream/tables/
+if [[ "$TGT_MAC" == "chrysalis" ]]; then
+    export DATA_DIR=/lcrc/group/e3sm/data/inputdata
+elif [[ "$TGT_MAC" == "pm-cpu" ]]; then
+    export DATA_DIR=/global/cfs/cdirs/e3sm/inputdata
+else
+    export DATA_DIR=${PREFIX}/share/pyeamxx/inputdata
+    # download this file because otherwise the build will fail...
+    wget https://web.lcrc.anl.gov/public/e3sm/inputdata/atm/scream/tables/p3_lookup_table_1.dat-v4.1.1
+    wget https://web.lcrc.anl.gov/public/e3sm/inputdata/atm/scream/tables/p3_lookup_table_2.dat-v4.1.1
+    mkdir -p ${PREFIX}/share/pyeamxx/inputdata/atm/scream/tables
+    mv p3_lookup_table_1.dat-v4.1.1 ${PREFIX}/share/pyeamxx/inputdata/atm/scream/tables
+    mv p3_lookup_table_2.dat-v4.1.1 ${PREFIX}/share/pyeamxx/inputdata/atm/scream/tables
+fi
 
 cmake -Wno-dev \
     -S ../../ \
@@ -14,6 +25,7 @@ cmake -Wno-dev \
     -DCMAKE_BUILD_TYPE='Release' \
     -DEAMXX_ENABLE_PYBIND='ON' \
     -DSCREAM_DOUBLE_PRECISION='OFF' \
+    -DSCREAM_ENABLE_MAM='OFF' \
     -DNetcdf_Fortran_PATH=$PREFIX \
     -DNetcdf_C_PATH=$PREFIX \
     -DCMAKE_CXX_FLAGS='-fvisibility-inlines-hidden -fmessage-length=0 -Wno-use-after-free -Wno-unused-variable -Wno-maybe-uninitialized' \
@@ -23,10 +35,10 @@ cmake -Wno-dev \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_Fortran_COMPILER=mpif90 \
     -DBUILD_SHARED_LIBS='ON' \
-    -DSCREAM_INPUT_ROOT="build_src" \
     -DSCREAM_ENABLE_BASELINE_TESTS='OFF' \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DPYTHON_EXECUTABLE=$PYTHON
+    -DPYTHON_EXECUTABLE=$PYTHON \
+    -DSCREAM_INPUT_ROOT=$DATA_DIR
 
 cmake --build build_src/src/python -j${CPU_COUNT:-128}
 
