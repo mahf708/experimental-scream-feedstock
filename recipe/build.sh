bcb204ca1377259474df2c6ed29e9958b01bbd20
mahf708 <naser.mahfouz@pnnl.gov>
2024-07-13 17:00:04 -0500

    pyscream 0.0.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,11 +10,11 @@ if [[ "$TGT_MAC" == "chrys" ]]; then
 elif [[ "$TGT_MAC" == "pmcpu" ]]; then
     export DATA_DIR=/global/cfs/cdirs/e3sm/inputdata
 else
-    export DATA_DIR=${PREFIX}/share/pyeamxx/inputdata
+    export DATA_DIR=${PREFIX}/share/pyscream/inputdata
     # download this file because otherwise the build will fail...
     wget https://web.lcrc.anl.gov/public/e3sm/inputdata/atm/scream/tables/p3_lookup_table_1.dat-v4.1.1
-    mkdir -p ${PREFIX}/share/pyeamxx/inputdata/atm/scream/tables
-    mv p3_lookup_table_1.dat-v4.1.1 ${PREFIX}/share/pyeamxx/inputdata/atm/scream/tables
+    mkdir -p ${PREFIX}/share/pyscream/inputdata/atm/scream/tables
+    mv p3_lookup_table_1.dat-v4.1.1 ${PREFIX}/share/pyscream/inputdata/atm/scream/tables
 fi
 
 cmake -Wno-dev \
@@ -22,14 +22,14 @@ cmake -Wno-dev \
     -B build_src \
     -DCMAKE_BUILD_TYPE='Release' \
     -DEAMXX_ENABLE_PYBIND='ON' \
-    -DSCREAM_DOUBLE_PRECISION='OFF' \
+    -DSCREAM_DOUBLE_PRECISION='ON' \
     -DSCREAM_ENABLE_MAM='OFF' \
     -DPIO_ENABLE_IO_STATS='OFF' \
     -DNetcdf_Fortran_PATH=$PREFIX \
     -DNetcdf_C_PATH=$PREFIX \
     -DCMAKE_CXX_FLAGS='-fvisibility-inlines-hidden -fmessage-length=0 -Wno-use-after-free -Wno-unused-variable -Wno-maybe-uninitialized' \
     -DCMAKE_C_FLAGS='' \
-    -DCMAKE_Fortran_FLAGS='-Wno-maybe-uninitialized -Wno-unused-dummy-argument' \
+    -DCMAKE_Fortran_FLAGS='-Wno-maybe-uninitialized -Wno-unused-dummy-argument -fallow-argument-mismatch' \
     -DCMAKE_CXX_COMPILER=mpic++ \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_Fortran_COMPILER=mpif90 \
@@ -41,9 +41,9 @@ cmake -Wno-dev \
 
 cmake --build build_src/src/python -j${CPU_COUNT:-128}
 
-find build_src -name "*.so*" | xargs cp -t libpyeamxx/
+find build_src -name "*.so*" | xargs cp -t libpyscream/
 
-for f in libpyeamxx/*.so*; do patchelf --set-rpath '$ORIGIN' --force-rpath $f; done
+for f in libpyscream/*.so*; do patchelf --set-rpath '$ORIGIN' --force-rpath $f; done
 
 $PYTHON -m build -w -n -x
 
@@ -51,6 +51,6 @@ pushd $SP_DIR
 unzip $SRC_DIR/scream/components/eamxx/src/python/dist/*.whl
 popd
 
-rm -rf ${PREFIX}/share/pyeamxx/inputdata/atm/cam
-rm -rf ${PREFIX}/share/pyeamxx/inputdata/atm/scream/init
-rm -rf ${PREFIX}/share/pyeamxx/inputdata/atm/scream/maps
+rm -rf ${PREFIX}/share/pyscream/inputdata/atm/cam
+rm -rf ${PREFIX}/share/pyscream/inputdata/atm/scream/init
+rm -rf ${PREFIX}/share/pyscream/inputdata/atm/scream/maps
